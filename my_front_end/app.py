c32528c244af3322cdd268e6652e4d77da8130cb
Hassan Ahmed <mr.hassaan95@gmail.com>
2018-03-20 16:31:36 +0500

    That is complete interfacing
    Complete interfacing of the project

--- my_front_end/app.py
+++ my_front_end/app.py
@@ -1,8 +1,48 @@
 from  flask import Flask, render_template
-project = Flask(__name__)
-@project.route('/hello')
-def index():
-    # return "hello word"
+app= Flask(__name__)
+@app.route('/')
+def home():
   return  render_template('page_home3.html')
+
+@app.route('/about')
+def about():
+    return render_template('page_about3.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('page_profile_projects.html')
+
+@app.route('/login')
+def login():
+    return render_template('page_login.html')
+
+
+@app.route('/registeration')
+def registration():
+    return render_template('page_registration.html')
+
+@app.route('/profile')
+def profile_main():
+    return render_template('page_profile.html')
+
+@app.route('/my_projects')
+def my_projects():
+    return render_template('page_profile_projects.html')
+
+@app.route('/my_history')
+def my_history():
+    return render_template('page_profile_history.html')
+
+
+
+@app.route('/my_profile')
+def my_profile():
+    return render_template('page_profile_me.html')
+
+
+
+
+
+
 if __name__ ==('__main__'):
-    project.run(debug=True)
+    app.run(debug=True)
